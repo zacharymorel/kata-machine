2f63d2b65d038db82b7504e0a2c088f47512c8bf
zacharymorel <zacharyjmorel@gmail.com>
2024-03-01 16:56:01 -0500

    LinearSearch.

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,17 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
+/**
+ * We have array ['a', 'b', 'c']
+ * search(arr, v)
+ * We walk linearly through arr until we find "v".
+ *
+ * Big O = O(n)
+ */
 
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) return true;
+    }
+    return false;
 }
