811dfaebb13b17a5d62681d55be80776d46cad76
zacharymorel <zacharyjmorel@gmail.com>
2024-03-04 07:30:46 -0500

    Binary and twoCrystal ball

--- src/__tests__/TwoCrystalBalls.ts
+++ src/__tests__/TwoCrystalBalls.ts
@@ -1,9 +1,13 @@
 import two_crystal_balls from "@code/TwoCrystalBalls";
 
 test("two crystal balls", function () {
+    // random number where we can set the array to true.
     let idx = Math.floor(Math.random() * 10000);
+
+    // fill array with false.
     const data = new Array(10000).fill(false);
 
+    // From after random number generated in var idx, make indexs value true.
     for (let i = idx; i < 10000; ++i) {
         data[i] = true;
     }
@@ -11,4 +15,3 @@ test("two crystal balls", function () {
     expect(two_crystal_balls(data)).toEqual(idx);
     expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
 });
-
--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    while (lo < hi) {
+        // middle
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+
+        // WE FOUND IT
+        if (v === needle) return true;
+        // We half the search for both conditions.
+        else if (v > needle) hi = m; // Move the search to the left.
+        else lo = m + 1; // Move the search to the right.
+    }
+
+    return false;
 }
