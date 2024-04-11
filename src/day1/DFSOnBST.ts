0369af86e917405f22464d7d80af8cc415dc0b74
zacharymorel <zacharyjmorel@gmail.com>
2024-04-11 16:50:00 -0400

    DFS Tree Binary Search

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,16 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+    // BASE CASE
+    if (!curr) return false;
 
+    if (curr.value === needle) return true;
+
+    // RECURSE // Binary search so we only have to search one side each time.
+    if (curr.value < needle) return search(curr.right, needle);
+    else return search(curr.left, needle);
+
+    // POST
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
