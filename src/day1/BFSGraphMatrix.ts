e98ff0785a796e54ef9731d588f89b95e262fbf7
zacharymorel <zacharyjmorel@gmail.com>
2024-06-27 16:54:01 -0400

    Worked through Graph traversals

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,58 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    // What we need to know:
+    // 1. We need to know where we are starting from "source".
+    // 2. We need to know what we are looking for "needle".
+    // 3. We want to be able to return the path we took.
+    // IN a graph, when we are doing a search, we often want to know the path associated with it.
 
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
+
+    seen[source] = true;
+    const q: number[] = [source];
+
+    do {
+        const curr = q.shift() as number;
+        if (curr === needle) {
+            break;
+        }
+        const adjs = graph[curr];
+        for (let i = 0; i < graph.length; i++) {
+            // walk through and grap each edge
+            if (adjs[i] === 0) {
+                // if no edge
+                continue;
+            }
+
+            if (seen[i]) {
+                // we've already seen the edge before
+                continue;
+            }
+
+            seen[i] = true;
+            prev[i] = curr;
+            q.push(i);
+        }
+        seen[curr] = true;
+    } while (q.length);
+
+    // Build it backwards
+    let curr = needle;
+    const out: number[] = [];
+    // Walk back until we hit -1 for path
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+
+    if (out.length) {
+        return [source].concat(out.reverse());
+    }
+
+    // prev[needle] === -1
+    return null;
 }
--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,62 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    // pre && base cases
+    //
+    // we've already seen this and we don't want to be here.
+    if (seen[curr]) {
+        return false;
+    }
 
+    // We've now seen current
+    seen[curr] = true;
+
+    // Add to the path so we record the path we took.
+    path.push(curr);
+    // console.log("PATH PUSH| vertex: ", path[path.length - 1]);
+
+    // WE FOUND IT
+    if (curr === needle) {
+        console.log("found needle path: " + curr, path);
+        return true;
+    }
+
+    // recurse
+    const edges = graph[curr];
+    // edges is an arraylist of objects "edges" that has a to and weight.
+    for (let i = 0; i < edges.length; i++) {
+        const edge = edges[i];
+
+        if (walk(graph, edge.to, needle, seen, path)) {
+            // we found the needle.
+            console.log("recurse found needle path: " + curr, path);
+            return true;
+        }
+    }
+
+    // post
+    // console.log("PATH POPPED| vertex: ", path[path.length - 1]);
+    console.log("post path: " + curr, path);
+    path.pop();
+
+    return false;
+}
+
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = new Array();
+
+    walk(graph, source, needle, seen, path);
+
+    if (path.length === 0) return null;
+    console.log("END path: ", path);
+    return path;
 }
